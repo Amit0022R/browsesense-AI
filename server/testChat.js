b3126da0cc37a619e9181ac8d0877dbda2a317db
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2026-04-24 12:05:40 +0530

    add test file

---
+++ server/testChat.js
@@ -0,0 +1,13 @@
+const data = [
+  { url: "youtube.com", visitCount: 10 },
+  { url: "github.com", visitCount: 5 }
+];
+
+fetch('http://localhost:3000/chat', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ question: "Where did I waste time?", history: data })
+})
+.then(res => res.json())
+.then(data => console.log(JSON.stringify(data, null, 2)))
+.catch(err => console.error(err));
